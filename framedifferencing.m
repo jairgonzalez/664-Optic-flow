8354305d30b10a988d5467ba86d0bc15c55f8d49
Jair Gonzalez <jairgonzalez@gmail.com>
2021-03-30 17:15:10 -0700

    changes

---
+++ framedifferencing.m
@@ -0,0 +1,40 @@
+filename = '31secondzebrafish.mp4';
+mov = VideoReader(filename);
+numFrames = mov.NumberOfFrames;
+numFramesWritten = 0;
+finalvid = VideoWriter('zFishOutput.avi');
+open(finalvid);
+
+frames = read(mov);
+frameInfo = size(frames);
+height = frameInfo(2);
+width = frameInfo(1);
+pixels = width .* height;
+disp(pixels);
+
+for l = 1:(500)
+    temp = frames(:,:,:,l);
+    temp = im2gray(temp);
+    temp2 = frames(:,:,:,l + 1);
+    temp2 = im2gray(temp2);
+    temp3 = frames(:,:,:,l); % rgb frame
+    for w = 1:width
+        for h = 1:height
+            if(abs(temp2(w, h) - temp(w, h)) > 30) 
+                %temp(w, h) = 0; 
+                temp3(w, h) = 255; 255;
+
+            else 
+                %temp(w, h) = 255; 
+                %temp(w, h) = temp2(w, h);
+            end
+        end 
+    end
+    %imshow(temp);
+    %writeVideo(finalvid, temp);
+
+    imshow(temp3);
+    writeVideo(finalvid, temp3);
+end 
+
+close(finalvid);
